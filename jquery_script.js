48febf5b4541c3c236c85ecccfecf5bd53db38cb
Nyarkoh <nyarkosn@dhec.sc.gov>
2019-07-26 13:32:39 -0400

    Updated the map using flexbox

---
+++ jquery_script.js
@@ -0,0 +1,38 @@
+function graphSlidedown() {
+    $(document).ready(function() {
+        $('.chart').slideUp(1000)
+
+    })
+}
+
+
+function graphSlideUp() {
+    $(document).ready(function() {
+        $('.chart').slideDown(1000)
+
+    })
+}
+
+
+function togglegraph() {
+    $(document).ready(function() {
+        $('.chart').slideToggle(1000)
+
+    })
+}
+
+
+
+function toggleVolumeBtn() {
+    $(document).ready(function() {
+        $('.volume__calculator').slideToggle(500)
+
+    })
+}
+
+function SlideDownVolumeBtn() {
+    $(document).ready(function() {
+        $('.volume__calculator').slideUp(1000)
+
+    })
+}
